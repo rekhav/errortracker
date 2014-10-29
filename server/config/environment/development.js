ddf089d97a14487fc79d01b3de3ff84a8f95e982
rekhav <dishita.rekha@gmail.com>
2014-10-29 23:21:51 +0100

    first commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/angularapp-dev'
+  },
+
+  seedDB: true
+};
